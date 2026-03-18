d924c85fd1f3e44b60d6fdce3b63ea5e29faf8fb
BraiRou <food211@qq.com>
2026-03-18 20:09:13 +0800

    选区传输白色边缘平滑过渡，消除硬切割锯齿

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
  * Service Worker - KM Watercolor Palette 离线缓存
  */
-const CACHE_NAME = 'km-palette-v26';
+const CACHE_NAME = 'km-palette-v27';
 const CACHE_URLS = [
   './app.html',
   './style.css',
