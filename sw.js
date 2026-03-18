4b6941c05a6239f4e12f41d7c8cc44ee859a6741
BraiRou <food211@qq.com>
2026-03-18 20:27:17 +0800

    选区传输改用欧氏距离计算白色过渡，修复红蓝等颜色白边问题

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
  * Service Worker - KM Watercolor Palette 离线缓存
  */
-const CACHE_NAME = 'km-palette-v28';
+const CACHE_NAME = 'km-palette-v29';
 const CACHE_URLS = [
   './app.html',
   './style.css',
