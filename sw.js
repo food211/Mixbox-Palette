e517fdb93d0fafb4878d000f662c86d5938cde2b
BraiRou <food211@qq.com>
2026-03-22 13:50:49 +0800

    更新缓存v35

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
  * Service Worker - KM Watercolor Palette 离线缓存
  */
-const CACHE_NAME = 'km-palette-v34';
+const CACHE_NAME = 'km-palette-v35';
 const CACHE_URLS = [
   './app.html',
   './style.css',
