63704d69183c9a598fb98bf0842b30e622ac41cc
BraiRou <food211@qq.com>
2026-03-22 14:04:52 +0800

    修复颜料tooltip阻挡点击事件，添加pointer-events:none，更新缓存v38

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
  * Service Worker - KM Watercolor Palette 离线缓存
  */
-const CACHE_NAME = 'km-palette-v37';
+const CACHE_NAME = 'km-palette-v38';
 const CACHE_URLS = [
   './app.html',
   './style.css',
