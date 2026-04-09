ff32d3ea13728d3502f2892c5189e75dd897ec6a
BraiRou <food211@qq.com>
2026-04-09 12:42:43 +0800

    V1.3.3 使用说明折叠、放大模式准心修复、焦点卡住提示

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
  * Service Worker - KM Watercolor Palette 离线缓存
  */
-const CACHE_NAME = 'km-palette-v60';
+const CACHE_NAME = 'km-palette-v61';
 const CACHE_URLS = [
   './app.html',
   './css/base.css',
