7655b6444e9af687c09c29cfe5f94640541c8569
BraiRou <food211@qq.com>
2026-04-12 23:42:25 +0800

    更新SW版本到72

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
  * Service Worker - KM Watercolor Palette 离线缓存
  */
-const CACHE_NAME = 'km-palette-v71';
+const CACHE_NAME = 'km-palette-v72';
 const CACHE_URLS = [
   './app.html',
   './css/base.css',
