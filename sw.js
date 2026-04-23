7d2ad2f2b91d9184e35762022d33379d1f705ad4
BraiRou <food211@qq.com>
2026-04-23 18:09:45 +0800

    sw: CACHE_NAME v89 → v90，强制刷新缓存应用 iPad 优化

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
  * Service Worker - KM Watercolor Palette 离线缓存
  */
-const CACHE_NAME = 'km-palette-v89';
+const CACHE_NAME = 'km-palette-v90';
 const CACHE_URLS = [
   './app.html',
   './css/base.css',
